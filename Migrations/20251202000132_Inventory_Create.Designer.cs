88c5edcbe6c41dd0b8b7e3a69537cbd1dd030be1
OmarDiab12 <135867239+OmarDiab12@users.noreply.github.com>
2025-12-02 02:05:10 +0200

    - Add migration to add Latest models

--- Migrations/20251202000132_Inventory_Create.Designer.cs
+++ Migrations/20251202000132_Inventory_Create.Designer.cs
@@ -4,6 +4,7 @@ using ERP.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ERP.Migrations
 {
     [DbContext(typeof(ERPDBContext))]
-    partial class ERPDBContextModelSnapshot : ModelSnapshot
+    [Migration("20251202000132_Inventory_Create")]
+    partial class Inventory_Create
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
